5193ed1ed857ae1afad3d480a0e39bdd99e9df1a
Josh Weaver <joshweaver@breezechms.com>
2021-05-03 15:35:21 -0400

    fixes bad config

--- .storybook/main.js
+++ .storybook/main.js
@@ -23,20 +23,6 @@ module.exports = {
         config.module.rules.push({
             test: /\.css$/,
             use: ["style-loader", "css-loader?modules=true"],
-            options: {
-                modules: {
-                    compileType: "module",
-                    mode: "local",
-                    auto: true,
-                    exportGlobals: true,
-                    localIdentName: "[path][name]__[local]--[hash:base64:5]", // only use this long name in DEV!!!
-                    localIdentContext: path.resolve(__dirname, "src"),
-                    localIdentHashPrefix: "my-custom-hash",
-                    namedExport: true,
-                    exportLocalsConvention: "camelCase",
-                    exportOnlyLocals: false,
-                },
-            },
             include: path.resolve(__dirname, "../"),
         });
 
